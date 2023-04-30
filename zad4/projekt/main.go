ccd3564e9cddea9eac2edf7f1a24f670655d5fd3
Kacper <kacper.kubit@student.uj.edu.pl>
2023-04-30 15:23:16 +0200

    na 3.5

--- zad4/projekt/main.go
+++ zad4/projekt/main.go
@@ -15,28 +15,23 @@ func main() {
 	e.Use(middleware.Logger())
 	e.Use(middleware.Recover())
 
-	// Inicjalizacja bazy danych
 	db, err := gorm.Open(sqlite.Open("products.db"), &gorm.Config{})
 	if err != nil {
 		panic("failed to connect to database")
 	}
 
-	// Migracja bazy danych
 	err = db.AutoMigrate(&product.Product{})
 	if err != nil {
 		panic("failed to migrate database")
 	}
 
-	// Inicjalizacja kontrolera produktów
 	pc := product.NewController(db)
 
-	// Dodaj obsługę routingu produktów
 	e.GET("/products", pc.GetAll)
 	e.POST("/products", pc.Create)
 	e.GET("/products/:id", pc.GetByID)
 	e.PUT("/products/:id", pc.Update)
 	e.DELETE("/products/:id", pc.Delete)
 
-	// Uruchom serwer Echo
 	e.Start(":8080")
 }
