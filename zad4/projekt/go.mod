cfd32e9543dba31f67137c6d4b9210283fc57ebe
Kacper <kacper.kubit@student.uj.edu.pl>
2023-04-30 15:21:27 +0200

    na 3.5

--- zad4/projekt/go.mod
+++ zad4/projekt/go.mod
@@ -6,6 +6,7 @@ require (
 	github.com/golang-jwt/jwt v3.2.2+incompatible // indirect
 	github.com/jinzhu/gorm v1.9.16 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
+	github.com/jinzhu/now v1.1.5 // indirect
 	github.com/labstack/echo/v4 v4.10.2 // indirect
 	github.com/labstack/gommon v0.4.0 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
@@ -18,4 +19,6 @@ require (
 	golang.org/x/sys v0.7.0 // indirect
 	golang.org/x/text v0.9.0 // indirect
 	golang.org/x/time v0.3.0 // indirect
+	gorm.io/driver/sqlite v1.5.0 // indirect
+	gorm.io/gorm v1.25.0 // indirect
 )
--- zad4/projekt/main.go
+++ zad4/projekt/main.go
@@ -1,21 +1,42 @@
 package main
 
 import (
-	controllers "controllers"
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+
+	"projekt/product"
 )
 
 func main() {
 	e := echo.New()
+
 	e.Use(middleware.Logger())
 	e.Use(middleware.Recover())
 
-	e.GET("/products", controllers.GetAllProducts)
-	e.GET("/products/:id", controllers.GetProductByID)
-	e.POST("/products", controllers.CreateProduct)
-	e.PUT("/products/:id", controllers.UpdateProduct)
-	e.DELETE("/products/:id", controllers.DeleteProduct)
+	// Inicjalizacja bazy danych
+	db, err := gorm.Open(sqlite.Open("products.db"), &gorm.Config{})
+	if err != nil {
+		panic("failed to connect to database")
+	}
 
+	// Migracja bazy danych
+	err = db.AutoMigrate(&product.Product{})
+	if err != nil {
+		panic("failed to migrate database")
+	}
+
+	// Inicjalizacja kontrolera produktów
+	pc := product.NewController(db)
+
+	// Dodaj obsługę routingu produktów
+	e.GET("/products", pc.GetAll)
+	e.POST("/products", pc.Create)
+	e.GET("/products/:id", pc.GetByID)
+	e.PUT("/products/:id", pc.Update)
+	e.DELETE("/products/:id", pc.Delete)
+
+	// Uruchom serwer Echo
 	e.Start(":8080")
 }
