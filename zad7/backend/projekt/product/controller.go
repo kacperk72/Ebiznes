3f954846cf14f2f905ebe55daf0b98dedc5831f0
Kacper <kacper.kubit@student.uj.edu.pl>
2023-05-30 17:37:59 +0200

    linter fixes and screens

--- zad7/backend/projekt/product/controller.go
+++ zad7/backend/projekt/product/controller.go
@@ -8,32 +8,40 @@ import (
 	"gorm.io/gorm"
 )
 
+// UnsetProductID represents a product ID value when it's unset or not found.
+const UnsetProductID = 0
+
+// Controller handles the routes related to products.
 type Controller struct {
 	db *gorm.DB
 }
 
+// NewController creates a new product controller with the given database.
 func NewController(db *gorm.DB) *Controller {
 	return &Controller{db}
 }
 
+// GetAll retrieves all the products.
 func (c *Controller) GetAll(ctx echo.Context) error {
 	var products []Product
 	c.db.Find(&products)
 	return ctx.JSON(http.StatusOK, products)
 }
 
+// GetByID retrieves a product with the given id.
 func (c *Controller) GetByID(ctx echo.Context) error {
 	id, _ := strconv.Atoi(ctx.Param("id"))
 	var product Product
 	c.db.First(&product, id)
 
-	if product.ID == 0 {
+	if product.ID == UnsetProductID {
 		return ctx.NoContent(http.StatusNotFound)
 	}
 
 	return ctx.JSON(http.StatusOK, product)
 }
 
+// Create creates a new product with the data from the request.
 func (c *Controller) Create(ctx echo.Context) error {
 	product := new(Product)
 	if err := ctx.Bind(product); err != nil {
@@ -44,12 +52,13 @@ func (c *Controller) Create(ctx echo.Context) error {
 	return ctx.JSON(http.StatusCreated, product)
 }
 
+// Update updates a product with given id.
 func (c *Controller) Update(ctx echo.Context) error {
 	id, _ := strconv.Atoi(ctx.Param("id"))
 	var product Product
 	c.db.First(&product, id)
 
-	if product.ID == 0 {
+	if product.ID == UnsetProductID {
 		return ctx.NoContent(http.StatusNotFound)
 	}
 	newProduct := new(Product)
@@ -64,13 +73,14 @@ func (c *Controller) Update(ctx echo.Context) error {
 	return ctx.JSON(http.StatusOK, product)
 }
 
+// Delete deletes a product with given id.
 func (c *Controller) Delete(ctx echo.Context) error {
 	id, _ := strconv.Atoi(ctx.Param("id"))
 
 	var product Product
 	c.db.First(&product, id)
 
-	if product.ID == 0 {
+	if product.ID == UnsetProductID {
 		return ctx.NoContent(http.StatusNotFound)
 	}
 
--- zad7/backend/projekt/product/model.go
+++ zad7/backend/projekt/product/model.go
@@ -4,6 +4,7 @@ import (
 	"gorm.io/gorm"
 )
 
+// Product represents a product with a name and price.
 type Product struct {
 	gorm.Model
 	Name  string `json:"name"`
