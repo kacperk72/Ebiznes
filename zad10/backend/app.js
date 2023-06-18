d1aa6a09f25b411f3ae1342e85137702c72d364d
Kacper Kubit <kacper.kubit99@gmail.com>
2023-06-18 21:55:45 +0200

    zad10 changes

--- zad10/backend/app.js
+++ zad10/backend/app.js
@@ -9,26 +9,28 @@ app.use(express.json());
 app.use(cors());
 
 app.post('/chat', (req, res) => {
-    console.log("zapytanie leci")
-    const userMessage = req.body.content;
+    // console.log("zapytanie leci")
+    // const userMessage = req.body.content;
 
-    const python = spawn('python', ['./main.py', userMessage]);
+    // const python = spawn('python', ['./main.py', userMessage]);
 
-    python.stdout.on('data', (data) => {
-        console.log("odpowiedz", data)
-        res.send({content: data.toString()});
-    });
+    // python.stdout.on('data', (data) => {
+    //     console.log("odpowiedz", data)
+    //     res.send({content: data.toString()});
+    // });
 
-    python.stderr.on('data', (data) => {
-        console.log(`stderr: ${data}`);
-        res.status(500).send({content: `Error: ${data}`});
-    });
+    // python.stderr.on('data', (data) => {
+    //     console.log(`stderr: ${data}`);
+    //     res.status(500).send({content: `Error: ${data}`});
+    // });
 
-    python.on('close', (code) => {
-        if (code !== 0) {
-            console.log(`python script exited with code ${code}`);
-        }
-    });
+    // python.on('close', (code) => {
+    //     if (code !== 0) {
+    //         console.log(`python script exited with code ${code}`);
+    //     }
+    // });
+
+    res.send("Example chat message")
 });
 
 const port = 3000;
